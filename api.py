ae7782a23a107a91df06ebc5bfaa971bf88f518f
maxwell <mullinmax@gmail.com>
2018-10-13 10:49:15 -0400

    port argument

--- api.py
+++ api.py
@@ -1,10 +1,14 @@
 from flask import Flask
-
+import sys
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "Hello, World!"
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = sys.argv[1]
+    print("Running on port: " + port)
+    app.run(port=port)
